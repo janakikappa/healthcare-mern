67f96fcae1cb398111332d18223b4b2693adbe76
Tejaswini20082007 <tejaswinisuru20@gmail.com>
2026-05-19 18:46:48 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://healthcare-mern-app-backend.onrender.com' });
+const API = axios.create({ baseURL: 'https://healthcare-mern-app-backend.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const info = JSON.parse(localStorage.getItem('userInfo') || 'null');
